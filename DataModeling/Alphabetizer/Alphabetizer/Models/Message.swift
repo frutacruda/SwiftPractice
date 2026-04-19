dfcc173cb2323778cc6a33c4647cd7ee27bb3f45
frutacruda <s2jwchoi@gmail.com>
2026-04-19 12:39:20 +0900

    week2 2

---
+++ DataModeling/Alphabetizer/Alphabetizer/Models/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  Alphabetizer
+//
+//  Created by jardin on 4/15/26.
+//
+import Foundation
+
+enum Message: String {
+    case instructions = "Place the tiles in alphabetical order"
+    case tryAgain = "Almost 😅"
+    case youWin = "You win! 🏆"
+}
